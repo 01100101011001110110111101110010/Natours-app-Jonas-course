ced73c6290c49ff9fef1f5a4028c4627dc604c33
01100101011001110110111101110010 <kolokolnikovea@gmail.com>
2026-02-27 12:11:50 +0700

    Processing of raw routes

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-dotenv.config({ path: "./config.env" });
-const app = require("./app");
+dotenv.config({ path: './config.env' });
+const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
-  "<PASSWORD>",
-  process.env.DATABASE_PASSWORD
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
 );
 mongoose.connect(DB, {}).then(() => {
-  console.log("DB connection successful!");
+  console.log('DB connection successful!');
 });
 
 const port = process.env.PORT || 3000;
 
-console.log(app.get("env"));
+console.log(app.get('env'));
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
