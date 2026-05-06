b3a1755ad359e9cefe8ed376a64e4e03871318ac
01100101011001110110111101110010 <kolokolnikovea@gmail.com>
2026-05-06 17:02:11 +0700

    Adding a ME endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,17 +12,6 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
-exports.getAllUsers = catchAsync(async (req, res) => {
-  const users = await User.find();
-  res.status(200).json({
-    status: 'success',
-    results: users.length,
-    data: {
-      users,
-    },
-  });
-});
-
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Создать ошибку, если пользователь POSTs пароль
   if (req.body.password || req.body.passwordConfirm) {
@@ -49,6 +38,11 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
 exports.deleteMe = catchAsync(async (req, res, next) => {
   await User.findByIdAndUpdate(req.user.id, { active: false });
   res.status(204).json({
@@ -57,20 +51,15 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This rout is not yet define!',
-  });
-};
-
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This rout is not yet defined!',
+    message: 'This route is not yet defined! Please use /signup instead',
   });
 };
 
+exports.getUser = factory.getOne(User);
+exports.getAllUsers = factory.getAll(User);
 // Не использовать для обновления пароля
 exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
