6d69531ebdec80354a9ff143cc4e8ec66dbb75bf
01100101011001110110111101110010 <kolokolnikovea@gmail.com>
2026-05-09 15:10:05 +0700

    Improving read perfomance with indexes

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -66,10 +66,7 @@ exports.getAll = (Model) =>
     let filter = {};
     if (req.params.tourId) filter = { tour: req.params.tourId };
 
-    const features = new APIFeatures(
-      Model.find(filter),
-      req.query,
-    )
+    const features = new APIFeatures(Model.find(filter), req.query)
       .filter()
       .sort()
       .limitFields()
