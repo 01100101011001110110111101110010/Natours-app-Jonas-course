a3a5c27c358495c1f07de669949402cb8042dfbf
01100101011001110110111101110010 <kolokolnikovea@gmail.com>
2026-05-21 11:02:14 +0700

    Mapbox added, display login

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -16,10 +16,16 @@ exports.getOverview = catchAsync(async (req, res, next) => {
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
-    fields: 'review rating user',
+    select: 'review rating user',
   });
   res.status(200).render('tour', {
     title: `${tour.name} Tour`,
     tour,
   });
 });
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account',
+  });
+};
--- routes/viewRouts.js
+++ routes/viewRouts.js
@@ -7,3 +7,4 @@ module.exports = router;
 
 router.get('/', viewController.getOverview);
 router.get('/tour/:slug', viewController.getTour);
+router.get('/login', viewController.getLoginForm);
