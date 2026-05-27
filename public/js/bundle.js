d1a375c4339ecbe9dae7b1e7f600394dccc7aa47
01100101011001110110111101110010 <kolokolnikovea@gmail.com>
2026-05-27 10:29:56 +0700

    Create complicate email handler

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -14392,7 +14392,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55850" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59640" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
